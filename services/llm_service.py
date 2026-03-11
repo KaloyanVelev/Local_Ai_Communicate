e09731695a036693ff2f4453b7dec9102fcbdf6b
Kaloyan Velev <kaloqnvelev1@gmail.com>
2026-03-11 16:14:41 +0200

    Add AI chat service using LMStudio

--- services/llm_service.py
+++ services/llm_service.py
@@ -1,6 +1,9 @@
 import requests
 
-OLLAMA_BASE_URL = "http://localhost:11434"
+OLLAMA_BASE_URL = "http://localhost:11434" # Ollama service URL
+LMSTUDIO_BASE_URL = "http://localhost:1234" # LMStudio service URL
+
+#choose one of the services at the end of the file
 
 class OllamaService:
     def __init__(self, model_name: str = 'qwen3.5:9b'):
@@ -41,4 +44,45 @@ class OllamaService:
         except Exception as e:
             raise Exception(f"LLM error: {str(e)}")
 
-llm_service = OllamaService()
+class LMStudioService:
+    def __init__(self, model_name: str = 'qwen3.5:9b'):
+        self.model_name = model_name
+        self.url = f"{LMSTUDIO_BASE_URL}/v1/chat/completions"
+
+    def chat(self,user_message,system_prompt = None):
+        messages = []
+        if system_prompt:
+            messages.append({
+                "role": "system",
+                "content": system_prompt
+            })
+        messages.append({
+            "role": "user",
+            "content": user_message
+        })
+        payload = {
+            "model": self.model_name,
+            "messages": messages,
+            "stream": False
+        }
+        try:
+            response = requests.post(self.url, json=payload, timeout=300)
+            response.raise_for_status()
+
+            data = response.json()
+            content = data['choices'][0]['message']['content']
+            if not content:
+                raise Exception(f"LMStudio returned empty content: {data}")
+
+            return content
+        except requests.exceptions.ConnectionError:
+            raise Exception("LMStudio service is not running")
+        except requests.exceptions.Timeout:
+            raise Exception("LMStudio service timed out")
+        except Exception as e:
+            raise Exception(f"LLM error: {str(e)}")
+
+
+
+
+llm_service = LMStudioService() #choose LMStudioService or OllamaService im working on making it easier!
